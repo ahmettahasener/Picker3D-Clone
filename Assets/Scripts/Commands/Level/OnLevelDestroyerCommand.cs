ce2b6e70c712b51320f4e1c9b8e936537d8a2e52
ahmettahasener <tahaxsener@gmail.com>
2024-03-27 04:31:59 +0300

    Input System Completed.

--- Assets/Scripts/Commands/Level/OnLevelDestroyerCommand.cs
+++ Assets/Scripts/Commands/Level/OnLevelDestroyerCommand.cs
@@ -7,12 +7,12 @@ public class OnLevelDestroyerCommand
 {
     private Transform _levelHolder;
 
-    public OnLevelDestroyerCommand(Transform levelHolder)
+    internal OnLevelDestroyerCommand(Transform levelHolder)
     {
         _levelHolder = levelHolder;
     }
 
-    public void Execute()
+    internal void Execute()
     {
         if (_levelHolder.transform.childCount <= 0) return;
         Object.Destroy(_levelHolder.transform.GetChild(0).gameObject);
